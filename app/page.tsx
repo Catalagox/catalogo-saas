199428a74a97dd9f9ec56a4c0f395765106a1831
catalagox <catalagoxx@gmail.com>
2026-02-25 18:37:20 -0300

    Descripción del cambio

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-/*import Hero from "@/components/Hero";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
@@ -6,12 +6,6 @@ export default function Home() {
       <Hero />
     </main>
   );
-}*/
-
-export default function Home() {
-  return (
-    <main style={{ padding: 50 }}>
-      <h1>HOME FUNCIONANDO</h1>
-    </main>
-  );
 }
+
+
