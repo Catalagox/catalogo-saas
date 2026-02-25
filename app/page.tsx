6f4fe673f3e8585aba72d99f6389dc6d0aed2acb
catalagox <catalagoxx@gmail.com>
2026-02-25 16:27:43 -0300

    Test home ultra simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Hero from "@/components/Hero";
+/*import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
       <Hero />
+    </main>
+  );
+}*/
+
+export default function Home() {
+  return (
+    <main style={{ padding: 50 }}>
+      <h1>HOME FUNCIONANDO</h1>
     </main>
   );
 }
