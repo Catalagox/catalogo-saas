83355f81c38c7abd0d43652830eda7923facee5a
catalagox <catalagoxx@gmail.com>
2026-09-02 10:34:54 -0300

    Actualizo pelota mundial

--- supabase-image-loader.js
+++ supabase-image-loader.js
@@ -1,8 +1,16 @@
-const projectId = 'yhlqooguctlzorinsxde'
-
 export default function supabaseLoader({ src, width, quality }) {
   if (src.includes('supabase.co')) {
-    return `${src}?width=${width}&quality=${quality || 75}`
+    let optimizedUrl = src;
+    
+    // Cambia la ruta pública a la ruta de renderizado de Supabase
+    if (src.includes('/storage/v1/object/public/')) {
+      optimizedUrl = src.replace(
+        '/storage/v1/object/public/',
+        '/storage/v1/render/image/public/'
+      );
+    }
+    
+    return `${optimizedUrl}?width=${width}&quality=${quality || 75}`;
   }
-  return src
+  return src;
 }
